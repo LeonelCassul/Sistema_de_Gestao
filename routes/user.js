9047318bb5bf689830dd9eb7690f0a7ff6b77ffb
Leonel Cassul <leonelsalmos@gmail.com>
2022-12-04 18:32:12 +0100

    Bug Fixed

---
+++ routes/user.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const connection = require('../connection');
+const router = express.Router();
+
+router.post('/signup', (req, res)=>!{
+    let user = req.body;
+    query 
+
+
+module.exports = route
