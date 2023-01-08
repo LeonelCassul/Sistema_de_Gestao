319220200be67fe4dc8e46f4757940438957f9ee
Leonel Cassul <leonelsalmos@gmail.com>
2023-01-08 18:26:00 +0100

    Instalacao do pacote npm install

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ const dashboardRoute = require('./routes/dashboard');
 const app = express();
 
 app.use('/dashboard', dashdashboardRoute)
-
-
 app.use(cors());
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
